074b334e56a04b09b28510ecf5cfa9b265a1790d
Diavor <diavor@MacBook-Pro.local>
2021-02-02 22:53:13 +0200

    Home Screen Products Listing

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -5,9 +5,23 @@ const Product = ({ product }) => {
   return (
     <Card className="my-3 p-3 rounded">
       <a href={`/product/${product._id}`}>
-        {/* <Card.Img src={product.img} variant="top" /> */}
-        <h1>{product.brand}</h1>
+        <Card.Img src={product.img} variant="top" />
       </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          <div className="my-3">
+            {product.rating} from {product.numReviews} revies
+          </div>
+        </Card.Text>
+
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
     </Card>
   );
 };
